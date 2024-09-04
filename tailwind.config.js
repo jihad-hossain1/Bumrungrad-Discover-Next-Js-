c86c33dd79ee2d11abcf7e18ac01b912ecd2d277
jihad hossain <jihadkhan934@gmail.com>
2024-09-04 22:46:11 +0600

    add basic setup -2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      blue: '#28266F',
+      green: '#128C45',
+      white: '#ffffff',
+      black: '#000000',
+      cream: '#DFE2F4',
+      red: '#FF0000',
+      ash: '#ACA6A5',
     },
+    extend: {},
   },
   plugins: [],
 };
