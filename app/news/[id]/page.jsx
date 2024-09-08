2532225b74647a3007232f54148ce5ebc2cd3d9f
jihad hosssain <jihadkhan@gmail.com>
2024-09-09 00:32:55 +0600

    news page added

---
+++ app/news/[id]/page.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React, { useEffect, useState } from "react";
+import CardLoader from "@/components/ui/cardLoader";
+import Image from "next/image";
+
+
+const OneNewsPage = ({params}) => {
+  const [loader, setLoader] = useState(true);
+  const [oneNews, setNews] = useState({});
+  useEffect(() => {
+    fetch(`https://api.discoverinternationalmedicalservice.com/api/get/news/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data.data);
+        setLoader(false);
+      });
+  }, [params.id]);
+  return (
+    <div className="p-5 my-5 md:container lg:w-[50%] md:mx-auto">
+      {loader ? (
+        <CardLoader cardLength={1} gridNumber={1} speed="slow" />
+      ) : (
+        <div className="flex flex-col gap-5">
+          <Image
+            height={300}
+            width={300}
+            src={oneNews?.newsImage}
+            alt="Bumrungrad International Hospital"
+            className="lg:h-[40vh] min-w-full"
+          />
+          <div className="">
+            <h5 className="font-semibold text-blue text-xl">
+              {oneNews?.newsTitle}
+            </h5>
+            <p className="my-3">{oneNews?.newsDescription}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OneNewsPage;
--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,11 +1,14 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { Divider } from '@mui/material'
 import Link from 'next/link'
-// import Loader from '../../shared/Loader/Loader'
 import Image from 'next/image'
-export default function News() {
-  const [newsData, setNewsData] = useState()
+import CardLoader from '@/components/ui/cardLoader'
+
+const NewsAll = () => {
+  const [allnewsData, setAllNewsData] = useState()
   const [loader, setLoader] = useState()
 
   useEffect(() => {
@@ -14,7 +17,7 @@ export default function News() {
       .then((res) => res.json())
       .then((data) => {
         if (data.status === 200) {
-          setNewsData(data?.data)
+          setAllNewsData(data?.data)
           setLoader(false)
         } else {
           console.log(data)
@@ -24,33 +27,28 @@ export default function News() {
   }, [])
   return (
     <div className='p-5 md:p-10 md:container md:mx-auto'>
-      <div className='flex justify-between items-center'>
-        <h2 className='capitalize text-xl md:text-2xl lg:text-3xl font-bold text-blue'>
+      <div className=''>
+        <h1 className='capitalize text-xl md:text-2xl lg:text-3xl font-bold text-blue'>
           Bumrungrad News
-        </h2>
-        <Link
-          href={'/news'}
-          className='rounded px-2 md:px-4 py-1 md:py-2 border border-blue text-blue hover:bg-blue hover:text-white duration-300 ease-linear'
-        >
-          View All
-        </Link>
+        </h1>
+        <div className='my-5'>
+          <Divider />
+        </div>
       </div>
       {loader ? (
-        <CardLoader cardLength={3} gridNumber={3} speed='slow' />
+        <CardLoader cardLength={9} gridNumber={3} speed='slow' />
       ) : (
         <div className='grid gap-5 md:grid-cols-2 lg:grid-cols-3 my-10'>
-          {newsData?.slice(0, 3).map((d, i) => (
+          {allnewsData?.map((d, i) => (
             <div
               key={i}
               className='shadow rounded hover:shadow-xl duration-300 ease-linear flex flex-col justify-between'
             >
               <Image
-                src={d.newsImage}
-                alt='Bumrungrad International Hospital'
-                effect='blur'
-                className=''
                 height={300}
                 width={300}
+                src={d.newsImage}
+                alt='Bumrungrad International Hospital'
               />
               <div className='p-4'>
                 {' '}
@@ -62,7 +60,6 @@ export default function News() {
                 </p>
               </div>
               <div className='p-4'>
-                {' '}
                 <Link href={`/news/${d?.id}`}>
                   <button className='border border-blue bg-blue hover:bg-white px-2 py-1 rounded hover:text-blue text-white duration-300 ease-linear'>
                     Read More
@@ -76,3 +73,5 @@ export default function News() {
     </div>
   )
 }
+
+export default NewsAll
