bba994eb000a8e1252b35a117bf6fac12350fd87
jihad hossain <jihadkhan934@gmail.com>
2024-09-13 22:21:33 +0600

    link add

--- app/(home)/_comp/journeyBetter.jsx
+++ app/(home)/_comp/journeyBetter.jsx
@@ -3,8 +3,27 @@
 import React from "react";
 import logo from "@/public/assets/Bumrungrad  Hospital_nav_logo.png";
 import Image from "next/image";
+import useAuth from "@/helpers/hooks/useAuth";
+import { useRouter } from "next/navigation";
 
 export default function JourneyBetter() {
+  const {auth} = useAuth();
+  const router = useRouter();
+
+  function handleAppointment() {
+    if (auth) {
+      router.push("/our-services/appointment");
+    } else {
+      router.push("/login");
+    }
+  }
+  function handleHealthScreen() {
+    if (auth) {
+      router.push("/check-up");
+    } else {
+      router.push("/login");
+    }
+  }
   return (
     <div className="p-4 md:p-8 mx-5 md:container md:mx-auto flex flex-col gap-5 md:gap-10 items-center bg-cream rounded shadow mt-5">
       <Image
@@ -25,10 +44,10 @@ export default function JourneyBetter() {
         services and world-class care.
       </p>
       <div className="flex flex-wrap gap-4 mt-5 justify-center">
-        <button className="text-sm px-4 py-2 bg-blue text-white border border-blue hover:bg-cream hover:text-blue md:hover:scale-105 ease-linear duration-300 shadow rounded">
+        <button onClick={handleAppointment} className="text-sm px-4 py-2 bg-blue text-white border border-blue hover:bg-cream hover:text-blue md:hover:scale-105 ease-linear duration-300 shadow rounded">
           Doctor Appoinment
         </button>
-        <button className="text-sm px-4 py-2 bg-blue text-white border border-blue hover:bg-cream hover:text-blue md:hover:scale-105 ease-linear duration-300 shadow rounded">
+        <button onClick={handleHealthScreen} className="text-sm px-4 py-2 bg-blue text-white border border-blue hover:bg-cream hover:text-blue md:hover:scale-105 ease-linear duration-300 shadow rounded">
           Health Screening
         </button>
         <button className="text-sm px-4 py-2 bg-blue text-white border border-blue hover:bg-cream hover:text-blue md:hover:scale-105 ease-linear duration-300 shadow rounded">
